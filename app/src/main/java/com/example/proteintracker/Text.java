18ebf30af74b372644a95eae25296addac04d3a1
YalinaHosea <yalinaa13@gmail.com>
2019-09-06 18:32:37 +0700

    tugas 3

---
+++ app/src/main/java/com/example/proteintracker/Text.java
@@ -0,0 +1,13 @@
+package com.example.proteintracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Text extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text);
+    }
+}
